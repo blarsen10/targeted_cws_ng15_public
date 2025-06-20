07faf508d9152f6489a5b63cee7a008bf6c84e4f
Bjorn Larsen <bjorn.larsen@yale.edu>
2025-06-20 12:40:11 -0400

    add setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='targeted_cws_ng15',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Running targeted CW analyses with enterprise',
+    author='Mingarelli Lab',
+    license='MIT',
+)
